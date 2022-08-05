6b7df92a0cc7df8f44a632efb595475c57960852
jacan99 <108701854+jacan99@users.noreply.github.com>
2022-08-05 18:36:23 -0500

    Add files via upload

---
+++ Tarea.cpp
@@ -0,0 +1,102 @@
+ #include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+int main(){
+
+string nombre,apellido,sexo,nacionalidad;
+int edad,ced;
+int opc;
+
+
+cout<<"\n1.\t\t\t\tBase de datos";
+
+cout<<"\nDigite la opcion 1 para ingresar sus datos Gracias:";
+cin>>opc;
+system ("cls");
+ if("opcion==1"){
+	
+	
+
+	cout<<"\t\t\t\tPersona numero 1";
+	
+	cout<<"\nEscribe tu nombre:";
+	cin>>nombre;
+	cout<<"\nEscribe tu apellido:";
+	cin>>apellido;
+	cout<<"\nEscribe tu edad:";
+	cin>>edad;
+	cout<<"\n\b ";getline(cin, sexo);
+    cout<<"\nEscribe tu sexo:";getline(cin, sexo);
+	cout<<"\nEscribe tu cedula:";
+	cin>>ced;
+	cout<<"\nEscribe tu nacionalidad:";
+	cin>>nacionalidad;
+	
+	cout<<"\t\t\t\tSus datos son:";
+	cout<<"\nnombre:"<<nombre<<endl;
+	cout<<"\napellido:"<<apellido<<endl;
+	cout<<"\nedad:"<<edad<<endl;
+	cout<<"\nsexo:"<<sexo<<endl;
+	cout<<"\nced:"<<ced<<endl;
+	cout<<"\nnacionalidad:"<<nacionalidad<<endl;
+	
+	cout<<"\t\t\t\tPersona numero 2";
+	
+	cout<<"\nEscribe tu nombre:";
+	cin>>nombre;
+	cout<<"\nEscribe tu apellido:";
+	cin>>apellido;
+	cout<<"\nEscribe tu edad:";
+	cin>>edad;
+	cout<<"\n\b ";getline(cin, sexo);
+    cout<<"\nEscribe tu sexo:";getline(cin, sexo);
+	cout<<"\nEscribe tu cedula:";
+	cin>>ced;
+	cout<<"\nEscribe tu nacionalidad:";
+	cin>>nacionalidad;
+	
+	cout<<"\t\t\t\tSus datos son:";
+	cout<<"\nnombre:"<<nombre<<endl;
+	cout<<"\napellido:"<<apellido<<endl;
+	cout<<"\nedad:"<<edad<<endl;
+	cout<<"\nsexo:"<<sexo<<endl;
+	cout<<"\nced:"<<ced<<endl;
+	cout<<"\nnacionalidad:"<<nacionalidad<<endl;
+	
+	cout<<"\t\t\t\tPersona numero 3";
+	
+	cout<<"\nEscribe tu nombre:";
+	cin>>nombre;
+	cout<<"\nEscribe tu apellido:";
+	cin>>apellido;
+	cout<<"\nEscribe tu edad:";
+	cin>>edad;
+	cout<<"\n\b ";getline(cin, sexo);
+    cout<<"\nEscribe tu sexo:";getline(cin, sexo);
+	cout<<"\nEscribe tu cedula:";
+	cin>>ced;
+	cout<<"\nEscribe tu nacionalidad:";
+	cin>>nacionalidad;
+	
+	cout<<"\t\t\t\tSus datos son:";
+	cout<<"\nnombre:"<<nombre<<endl;
+	cout<<"\napellido:"<<apellido<<endl;
+	cout<<"\nedad:"<<edad<<endl;
+	cout<<"\nsexo:"<<sexo<<endl;
+	cout<<"\nced:"<<ced<<endl;
+	cout<<"\nnacionalidad:"<<nacionalidad<<endl;
+	
+	cout<<"\nHasta Luego y Gracias por ingresar sus datos";
+	
+
+}
+
+ 	
+ return 0;
+}
+
+
